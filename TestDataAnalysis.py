5466437b856e075a24a6b0b8f72f89965a7f3961
David Wright <djwright3.14@gmail.com>
2018-10-15 14:20:55 -0500

    Significant class-based implementation.

--- TestDataAnalysis.py
+++ TestDataAnalysis.py
@@ -10,64 +10,155 @@ import csv
 import os
 import sys
 
+class Settings:
+    '''Program settings'''
+    # File path and filename settings
+    #folder = '/Users/your_username/your_folder/' # MacOS
+    #folder = 'C:\\Users\\your_home_folder\\your_folder' # Windows
+    folder = '/Users/davidwright/Dropbox/Cob/Data/MixRatioExperiment/MatrixRatios/'
+    base_name = '100-'
+    suffix = '.Dat'
+
+    # Read settings
+    files_to_read = 5
+    start_index = 1
+    index_digits = 3
+
+    x_axis = 'displacement'
+    #x_axis = 'strain'
+    y_axis = 'load'
+    #y_axis = 'stress'
+    # Compliance constants:
+    compliance = 0 # tensile testing
+    # compliance = 0.00005917356 # Three-point bending [mm/N]
+    compliance = 0.00001037912 # Flat Plate compression [mm/N]
+    modulus_lower_bound = 0.4
+    modulus_upper_bound = 0.75
+
+    ######## Input File Parameters
+    delimiter = ',' # CSV file
+    #delimiter = '\t' # tab separated file
+    header_lines = 4
+    time_column = None
+    load_column = 0
+    disp_column = 1
+    stress_column = 2
+    strain_column = 5
+
+class Analyst:
+    '''Class for running analysis on individual tests'''
+    def __init__(self,settings):
+        self.names = []
+        self.settings = settings
+        self.names = self.generate_filename_list()
+        self.data_sets = []
+        self.peaks = []
+        self.moduli = []
+        self.create_data_sets()
+
+    def generate_filename_list(self):
+        n = self.settings.files_to_read
+        filenames = [0]*n
+        for i in range(self.settings.start_index,n+self.settings.start_index):
+            number = str(i).zfill(self.settings.index_digits)
+
+            filename = self.settings.base_name + number
+            filenames[i-1] = filename
+        return filenames
+
+    def set_dir(self):
+        folder = self.settings.folder
+        exists = False
+        is_folder = False
+        if os.path.exists(folder):
+            exists = True
+        else:
+            print('Filepath specified does not exist: ' + folder)
+            return
+        if os.path.isdir(folder):
+            is_folder = True
+        else:
+            print('Filepath specified is not a directory: ' + folder)
+            return
+        if exists and is_folder:
+            os.chdir(folder)
+        return
+
+    def create_data_sets(self):
+        self.set_dir()
+        for filename in self.names:
+            self.data_sets.append(Test(filename,self.settings))
+        return
+
 class Test:
     '''Class containing test data and results'''
 
-    def __init__(self,name):
+    def __init__(self,name,settings):
+        self.settings = settings
         self.name = name
         self.data = []
         self.input_suffix = None
-        self.x_axis = 'Stress'
-        self.y_axis = 'Strain'
+
+        self.peak = None
+        self.modulus = None
+
+        self.x_axis = self.settings.x_axis
+        self.y_axis = self.settings.y_axis
         self.x_data = None
         self.y_data = None
+        self.lower_thresh=0.25
+        self.upper_thresh=0.75
+
+        self.read_data()
+        self.useful_data()
+        self.calculate_peak()
+        self.calculate_modulus()
 
     def useful_data(self):
-        if self.y_axis == 'Stress':
-            self.y_data = [point.stress for point in data]
-        elif self.y_axis == 'Load':
-            self.y_data = [point.load for point in data]
+        if self.y_axis == 'stress':
+            self.y_data = [point.stress for point in self.data]
+        elif self.y_axis == 'load':
+            self.y_data = [point.load for point in self.data]
         else:
             print('Unknown y axis parameter encountered in calculate_peak: '
                 + self.y_axis)
 
-        if self.x_axis == 'Strain':
-            self.x_data = [point.strain for point in data]
-        elif self.x_axis == 'Displacement':
-            self.x_data = [point.displacement for point in data]
+        if self.x_axis == 'strain':
+            self.x_data = [point.strain for point in self.data]
+        elif self.x_axis == 'displacement':
+            self.x_data = [point.displacement for point in self.data]
         else:
             print('Unknown x axis parameter encountered in calculate_peak: '
                 + self.x_axis)
+        return
 
     def add_data_point(self,point):
         self.data.append(point)
-
-    def peak(self):
-        # if self.peak != nil, return value, otherwise calculate value
-        if self.peak == None:
-            self.peak = self.calculate_peak()
-        return self.peak
 
     def calculate_peak(self):
         '''Calculates the peak of a given column from data'''
         peak = max(self.y_data)
         return peak
 
-    def modulus(self):
-        pass
+    def calculate_modulus(self):
+        lower = self.get_modulus_point(self.lower_thresh)
+        upper = self.get_modulus_point(self.upper_thresh)
+        # get slope at d_load/d_disp
+        slope = (upper[0] - lower[0])/(upper[1]-lower[1])
+        return slope
 
-    def get_modulus_point(self,data,thresh):
-        load_max = get_max_load(data)
-        load_target = thresh*load_max
-        index = get_nearest_index(load_target,data[0])
-        load_actual = data[0][index]
-        disp = data[1][index]
-        return [load_actual,disp]
+    def get_modulus_point(self,thresh):
+        max = self.peak
+        y_target = thresh*max
+        index = get_nearest_index(y_target,self.y_data)
+        y = self.y_data[index]
+        x = self.x_data[index]
+        return [y,x]
 
     def get_nearest_index(self,value,list):
+        '''Gets the index of the first item from list closest to target value'''
         index = -1
         for i in range(0,len(list)):
-
             if list[i] == value:
                 index = i
                 break
@@ -85,8 +176,41 @@ class Test:
         if index == -1:
             print('Value %f not found in list'%(value))
             return
-
         return index
+
+    def input_filename(self):
+        input_filename = self.name + self.settings.suffix
+        return input_filename
+
+    def read_float(self,index,to_read):
+        if index is None:
+            return None
+        else:
+            try:
+                return float(to_read[index])
+            except:
+                print('Float conversion error')
+
+    def read_data(self):
+        filename = self.input_filename()
+        delimiter = self.settings.delimiter
+        with open(filename, 'r') as fin:
+            for i in range(0,self.settings.header_lines):
+                next(fin)  # skip headings
+            if delimiter == ',':
+                reader = csv.reader(fin)
+            else:
+                reader = csv.reader(fin, delimiter=delimiter)
+
+            for line in reader:
+                this_point = DataPoint()
+                this_point.load = self.read_float(self.settings.load_column,line)
+                this_point.displacement = self.read_float(self.settings.disp_column,line)
+                this_point.stress = self.read_float(self.settings.stress_column,line)
+                this_point.strain = self.read_float(self.settings.strain_column,line)
+                this_point.time = self.read_float(self.settings.time_column,line)
+                self.add_data_point(this_point)
+        return
 
 class DataPoint:
     '''Single test data point (row) read from file'''
@@ -96,172 +220,6 @@ class DataPoint:
         self.displacement = None
         self.stress = None
         self.strain = None
-
-def generate_filename_list(basename,suffix,n,digits,start_index=1):
-    filenames = [0]*n
-    for i in range(start_index,n+start_index):
-        number = str(i).zfill(digits)
-
-        filename = basename + number + suffix
-        filenames[i-1] = filename
-    return filenames
-
-def read_files(folder,filenames,delimiter=',',kind = 'stress-strain'):
-    exists = False
-    is_folder = False
-    if os.path.exists(folder):
-        exists = True
-    else:
-        print('Filepath specified does not exist: ' + folder)
-        return
-    if os.path.isdir(folder):
-        is_folder = True
-    if exists and is_folder:
-        os.chdir(folder)
-
-    data_sets = []
-    for filename in filenames:
-        data = readtensiledata(filename,delimiter=delimiter,kind=kind)
-        data_sets.append(data)
-
-    for i in range(0,len(data_sets)):
-        for j in range(0,len(data_sets[i])):
-            for k in range(0,len(data_sets[i][j])):
-                try:
-                    data_sets[i][j][k] = float(data_sets[i][j][k])
-                except Exception as ex:
-                    logging.exception('Caught an error')
-
-    return data_sets
-
-def readtensiledata(filename,delimiter = ',',kind = 'stress-strain'):
-
-    load_column = 0
-    disp_column = 0
-    if kind == 'load-displacement':
-        load_column = 0
-        disp_column = 1
-    elif kind == 'stress-strain':
-        load_column = 2
-        disp_column = 5
-    else:
-        print('Unknown kind of test selected in readtensiledata: ' + kind)
-        return []
-
-    ########## Data Importing ############################################
-    Load= []
-    Disp = []
-    Stress = []
-    Time = []
-    Temp = []
-    Axial = []
-    Transverse = []
-    RawLoad = []
-    Empty = []
-    columns = [Load, Disp,Stress,Time,Temp,Axial,Transverse,RawLoad,Empty]
-
-    with open(filename, 'r') as fin:
-        for i in range(0,4):
-            next(fin)  # skip headings
-        if delimiter == ',':
-            reader = csv.reader(fin)
-        else:
-            reader = csv.reader(fin, delimiter=delimiter)
-
-        for line in reader:
-            if len(line) != len(columns):
-                print('Error: Line has %i elements and %i columns specified'%(len(line),len(columns)))
-                return
-            for element, targetlist in zip(line,columns):
-                targetlist.append(element)
-    data_set = [columns[load_column],columns[disp_column]]
-    return data_set
-
-def get_max_loads(data_sets):
-    max_loads = []
-    for data in data_sets:
-        max_loads.append(get_max_load(data))
-    return max_loads
-
-def get_max_load(data):
-
-    load_data = data[0]
-    max_load = max(load_data)
-
-    return max_load
-
-def get_max_point(data):
-    max_load = get_max_load(data)
-    max_load_disp = data[1][get_nearest_index(max_load,data[0])]
-    return [max_load,max_load_disp]
-
-def compliance_correction(data,rate):
-    loads = data[0]
-    disps = data[1]
-    if len(loads) != len(disps):
-        print('Length of load and displacement data not matched')
-
-    for i in range(0,len(loads)):
-        load = loads[i]
-        disp = disps[i]
-        disp_corrected = disp - load*rate
-        disps[i] = disp_corrected
-
-    return [loads,disps]
-
-def get_moduli(data_sets,lower_thresh=0.25,upper_thresh=0.75,compliance_rate=0):
-    slopes = []
-    for data in data_sets:
-        if compliance_rate != 0:
-            data = compliance_correction(data,compliance_rate)
-        slopes.append(get_modulus(
-                data,
-                lower_thresh=lower_thresh,
-                upper_thresh=upper_thresh)
-            )
-
-    return slopes
-
-def get_modulus(data,lower_thresh=0.25,upper_thresh=0.75):
-
-    lower = get_modulus_point(data,lower_thresh)
-    upper = get_modulus_point(data,upper_thresh)
-    # get slope at d_load/d_disp
-    slope = (upper[0] - lower[0])/(upper[1]-lower[1])
-
-    return slope
-
-def get_modulus_point(data,thresh):
-    load_max = get_max_load(data)
-    load_target = thresh*load_max
-    index = get_nearest_index(load_target,data[0])
-    load_actual = data[0][index]
-    disp = data[1][index]
-    return [load_actual,disp]
-
-def get_nearest_index(value,list):
-    index = -1
-    for i in range(0,len(list)):
-
-        if list[i] == value:
-            index = i
-            break
-        if i > 0:
-            if list[i] > value and list[i-1] < value:
-                lower_val = list[i-1]
-                lower_error = lower_val - value
-                upper_val = list[i]
-                upper_error = upper_val - value
-                if abs(lower_error) < abs(upper_error):
-                    index = i-1
-                else:
-                    index = i
-                break
-    if index == -1:
-        print('Value %f not found in list'%(value))
-        return
-
-    return index
 
 ##################################################
 # Plotting functions
@@ -329,58 +287,24 @@ def plot_data(data,name,analysis=True,xLabel='x',yLabel='y',shouldShow=False,mod
 #########################
 ##### Begin script ######
 #########################
+my_settings = Settings()
+analyst = Analyst(my_settings)
+print(analyst.names)
 
-# File path and filename settings
-#folder = '/Users/your_username/your_folder/' # MacOS
-#folder = 'C:\\Users\\your_home_folder\\your_folder' # Windows
-folder = '/Users/davidwright/Dropbox/Cob/Data/MixRatioExperiment/MatrixRatios/'
-base_name = '100-'
-suffix = '.Dat'
-
-# Read settings
-files_to_read = 5
-start_index = 1
-index_digits = 3
-delimiter = ',' # CSV file
-#delimiter = '\t' # tab separated file
-
-test_kind = 'load-displacement'
-#test_kind = 'stress-strain'
-# Compliance constants:
-compliance = 0 # tensile testing
-# compliance = 0.00005917356 # Three-point bending [mm/N]
-compliance = 0.00001037912 # Flat Plate compression [mm/N]
-modulus_lower_bound = 0.4
-modulus_upper_bound = 0.75
-# Excecute analysis
-filenames = generate_filename_list(
-    base_name,
-    suffix,
-    files_to_read,
-    digits=index_digits,
-    start_index = start_index)
-
-data_sets = read_files(
-    folder,
-    filenames,
-    delimiter = delimiter,
-    kind = test_kind)
-
-if data_sets is None:
+if analyst.data_sets is None:
     sys.exit('No data sets read. Exiting.')
 
-max_loads = get_max_loads(data_sets)
-moduli = get_moduli(data_sets,
-    compliance_rate=compliance,
-    upper_thresh = modulus_upper_bound,
-    lower_thresh=modulus_lower_bound)
-plot_data_sets(data_sets,filenames,
-    kind=test_kind,
-    mod_upper = modulus_upper_bound,
-    mod_lower=modulus_lower_bound)
+#plot_data_sets(data_sets,filenames,
+#    kind=test_kind,
+#    mod_upper = modulus_upper_bound,
+#    mod_lower=modulus_lower_bound)
 
 # print results
+analyst.calculate_peaks()
+
 print('Max Loads:')
-[print(x) for x in max_loads]
-print('Slopes:')
-[print(x) for x in moduli]
+for x in analyst.data_sets:
+    print(x.peak)
+#[print(x) for x in analyst.data_sets]
+#print('Slopes:')
+#[print(x) for x in moduli]
